1970e4e0d599377cda9ae7832fd374209f84901e
Stuart Cannon <sc651@grace.ex.ac.uk>
2016-05-25 08:24:23 +0100

    removed old code

--- ExtractSequence.py
+++
@@ -1,8 +0,0 @@
-import xml.etree.ElementTree as tree
-
-LRGfile_tree = tree.parse('LRG_1.xml')
-
-root = LRGfile_tree.getroot()
-
-GeneSequence = list(root[0][7].text) 
-#this line of code extracts the genomic sequence of gene in the LRG file and stores it as a list (CM)
--- parserCode/XML_intron_parser.py
+++ parserCode/XML_intron_parser.py
@@ -1,16 +1,24 @@
 #import modules
 import xml.etree.ElementTree as tree
-from xml.etree.ElementTree import Element, tostring, SubElement
+from xml.etree.ElementTree import Element
 from lxml import etree
-import glob, sys, os, re
+import os, re
 
+#model the local workspace
 class Workspace(object):
-    lrg_directories = 0
-    lrg_workspace_list = []
-    lrg_non_workspace_list = []
     lrg_instances = []
       	
-    #initialise to current working directory
+    #initialise workspace to current working directory. Should be immediate parent directory of directory containing lrg directories
+    #workspace diagram:
+
+    #    parent directory/
+    #        lrgs/
+    #	        LRG_1/
+    #               LRG_1.xml
+    #           LRG_2/
+    #               LRG_2.xml
+
+
     def __init__(self, root_dir=None):
 	if root_dir is None:
 	    self.root_dir = os.getcwd()
@@ -20,23 +28,31 @@ class Workspace(object):
 	    print 'root directory not recognised by workspace __init__'
 
 
-    #method to find lrg pathnames and return array of lrg_instances, default to return all lrg_filepaths input of lrg paths
-    #takes a list of filepaths or returns all lrg files in the child directroy named 'lrgs'
-    def lrg_array(self, lrg_targets=None, force=False):
+    #find all lrg pathnames in child directories and returns array of lrg_instances,
+    def lrg_array(self):
+	#path to 'lrgs' directory containing all LRG directories
 	lrg_root = os.path.join(self.root_dir, 'lrgs')
-	#iterate_through sub-directories 
+	#iterate_through subdirectories, directories and files in the lrg_root
 	for subdirs, dirs, files in os.walk(lrg_root):
+	    #iterate through the files in each dirs
 	    for file in files:
+		#basic regular expression to match default names for lrg xml files
 		if re.match("LRG_.*xml", str(file)):
+		    #join file name to absolute path (subdirs)
 		    file = os.path.join(subdirs, file)
+		    #initialise and append an LRG instance for each file found
 		    self.lrg_instances.append(Lrg(file))
+		#basic regular expression to match default names for lrg fasta files
 		elif re.match("LRG_.*fasta", str(file)):
 		    file = os.path.join(subdirs, file)
 		    self.lrg_instances.append(Lrg(file))
+	#return array of LRG instances
 	return self.lrg_instances
 
+#models sections of an lrg_file
 class Lrg:
-
+    #requires the path to an lrg_file. Uses lxml to read the xml file into memeory and get the root
+    #all other instance variables are updateable using the appropriate functions as required
     def __init__(self, lrg_path):
 	self.path = lrg_path
 	self.lrg_tree = tree.parse(self.path)
@@ -45,16 +61,17 @@ class Lrg:
 	self.format = ""
 	self.id = ""
 	self.exon_coord = {}
-	self.exon_count = 0
 	self.intron_coord = {}
+	self.sequence = []
     
-    #get_lrg_name
+    #get filename without extension
     def get_name(self):
 	base = os.path.basename(self.path)
 	no_ext = os.path.splitext(base)
 	self.name = no_ext[0]
 	return self.name
 
+    #return format of lrg file
     def get_format(self):
 	base = os.path.basename(self.path)
 	ext = os.path.splitext(base)[1].strip(".")
@@ -64,30 +81,34 @@ class Lrg:
 	    self.format = ext
         return self.format
     
+    #return the lrg id/name from within the lrg file
     def get_id(self):
 	for id in self.lrg_root.iter('id'):
 	    self.id = id.text
 	return self.id
 
-    def get_exon(self):
+    #return {exon_number : [exon_start, exon_end]}
+    def get_exon_coords(self):
+	#string used in lrg files to label the exon number 
 	exon_label = 'label'
+	#iterate through ever element called 'exon'
 	for exon in self.lrg_root.iter('exon'):
 	    if 'label' in exon.attrib.keys():
+		#extract exon_number
 		exon_number = (exon.attrib['label'])
+		#iterate through coordinates in the child elements of each exon
 	        for coords in exon:
+		    #pull out coordinates used in the exon LRG genomic sequence as opposed to a transcript sequence
 		    if self.id in coords.attrib.values():
+			#update the instance variable dictionary with the extracted coordinates
 			self.exon_coord[exon_number] = [coords.attrib['start'], coords.attrib['end']]
-			self.exon_count += 1
 	return self.exon_coord 
 
-
-    def get_intron(self):
-	#iterate through exon number and coordinate list in exon_dict
+    #return {intron_number :
+    def get_intron_coords(self):
 	for exon_number, exon_coord_list in self.exon_coord.iteritems():
 	    exon_number = int(exon_number)
-	    #create a lsit to be appended with cordinates
 	    intron_coord_list = []
-	    #conditional for intron start - exon end unless it is the last exon
 	    if exon_number < len(self.exon_coord.keys()):
 		intron_number = exon_number
 		intron_start = int(exon_coord_list[1]) + 1
@@ -102,141 +123,24 @@ class Lrg:
 		    pass
 	    if exon_number < len(self.exon_coord.keys()):
 	        self.intron_coord[exon_number] = intron_coord_list
-	print self.intron_coord
 	return self.intron_coord
-		
+
+    def get_sequences(self):
+	for sequence in self.lrg_root.iter('sequence'):
+	    self.sequence.append(sequence.text)
+	return self.sequence    
 
 if __name__ == "__main__":
     WS = Workspace()
     lrg_instances = WS.lrg_array()
     for lrg in lrg_instances:
-	x = lrg.get_name()
-	y = lrg.get_format()
-	z = lrg.get_id()
-	a = lrg.get_exon()
-	b = lrg.get_intron()
-'''
-
-
-#parses a specified xml file, returns a dictionary (hash) { exon_number : [genomic_start, genomic_end]} 
-def lrg_exon_coordinates(lrg_tree):
-	#create an empty dictionary to be later appended
-        exon_dict = {}
-        #use for loop to identify all elements with the tag 'exon' 
-        for element in lrg_root.iter('exon'):
-		#assign the exon tag to a variable
-		exon_tag = element.tag
-		#assign the exon attibute dictionary to a variable
-		exon_attribute = element.attrib
-		#assign the string 'label' to a variable which can be used to ensure the code only pulls through the exons
-		exon_label = 'label'
-		#if statement to see if the exon_label is in the key of the exon attribute dictionary
-		if exon_label in exon_attribute.keys():
-			#assert number of keys in dictionary is equal to the number of exons 
-			#pull out the exon number from the atribute dictionary
-			exon_number = (exon_attribute[exon_label]) 
-			#iterate through the elements within the elements that pass the for loop i.e. only the exon numbers
-	                for child in element:
-				#this is the coordinate system we require of the three available
-				coordinate_label = "LRG_1"
-				coordinate_attribute = child.attrib
-				#ASSERT - check there are the same number of coordinate attributes as there are exons
-				#if statement to pull out only the LRG_1 coordinate system data by checking the label against the values in the generated dictionary
-				if coordinate_label in coordinate_attribute.values():
-					#assigning each coordinate start attribute to a variable
-					exon_start = coordinate_attribute['start']
-					#assigning each coordinate end attribute to a variable
-					exon_end = coordinate_attribute['end']
-					#generate a list of exon_start and exon_end for each exon
-					start_end_list = [exon_start, exon_end]
-					#append the exon_dictionary with the exon number as a key and the exon_start, exon_end as the values
-					exon_dict[int(exon_number)] = start_end_list
-	#make the output of the function the exon dictionay that has been created
-	return exon_dict
-
-""" Includes as function to infer the intron number, start and end coordinates by taking a dictionary in the format {'exon_number': 'exon_ start_ coordinate', 'exon_end_coordinate'}.
-Returns the intron dictionary in the format {'intron_number' : 'intron_start_coordinate', 'intron_end_coordinate'} for introns in between the exons in the given exon dictionary"""
-def LRG_intron_coordinates(exon_dict):
-	#generate the empty intron dictionary which will be later updated with intron number as the key and intron start, intron end coordinates as
-	#the values
-	intron_dict = {}
-	#iterates through every key in the exon_dictionary
-	for exon_number in exon_dict:
-		#if statement to exclude the final exon (as there will be one less introns than exons)
-		if exon_number != exon_dict.keys()[-1]:
-			#assign the new list of keys to the variable intron_number which will later become the key in the intron_dictionary
-			intron_number = exon_number
-			#ASSERT - check the number of introns is one less than the number of exons
-			#if statement to identify intron start value
-			if intron_number in exon_dict.keys(): 
-				#using intron number to only pull out values in the exon dictionary required to identify intron start (i.e. doesn't pull out the end values associated with the final exon)
-				value = (exon_dict[intron_number])
-				#intron start is the exon end coordinate(2nd value in the exon_dict) plus 1
-				intron_start = int(value[1]) + 1
-				#this excludes the first exon in the exon dictionary to enable the intron end values to be calculated. we do not want to know the start value of the last exon
-				exon_exclude_1 = intron_number + 1 
-				#if statement to identify intron end value by using the exon_exclude list to reference the exon_dictionary
-				if exon_exclude_1 in exon_dict.keys():
-					value = exon_dict[exon_exclude_1]
-					#the end intron is the intron start -1 (excludes the first intron as stated previously)
-					intron_end = int(value[0]) -1
-					#concatenate the intron start and end values for each intron into a list 
-					intron_start_end_list = [intron_start, intron_end]
-					#create a dictionary with intron number (1:n-1) as the key and intron start, intron end as the values.  
-					intron_dict[int(intron_number)] = intron_start_end_list
-	#assert - check the number of introns in the dictionary is one less than the number of exons in the exon dictionary 
-	return intron_dict
-
-"""Includes a function to parse the genomic intron sequences from an xml file by taking the genomic sequence and a dictionary in the format {'intron_number' : 'intron_start_coordinate', 'intron_end_coordinate'}"""
-def LRG_intron_sequence(intron_dict):
-        for intron_number in intron_dict:
-		value = (intron_dict[intron_number])
-		intron_sequence_start = int(value[0]) -1
-		intron_sequence_end = int(value[1])
-		intron_sequence = (GeneSequence[intron_sequence_start:intron_sequence_end])
-		intron_sequence = ''.join(intron_sequence)
-		intron_dict[intron_number].append(intron_sequence)
- 		# check the length of the sequence == (end - start) + 1
-#		print intron_dict
-	return intron_dict
-
-def xml_from_dict(tag, intron_dict):
-	filename = tag + "_intron_sequences.xml"
-	with open(filename, "w") as outfile:
-		root = etree.Element(tag)
-		for key, value in intron_dict.items():
-			intron_attribute = {"intron_start" : str(value[0]), "intron_number" : str(key), "intron_end" : str(value[1])}
-			intron_tag = etree.SubElement(root, 'intron', intron_attribute)
-			intron_tag.text = value[2]
-		s = etree.tostring(root, pretty_print=True)
-		outfile.write(s)
-
-"""uses glob.glob() to search the current folder for all LRG files in the format LRG_*.xml. Ensure LRG files retain t$
-will run the program on every LRG_file within the dictionary""" 
-
-if __name__ == "__main__" :
-    for filename in glob.glob('LRG_*.xml'):
-        lrg_tree = tree.parse(filename)
-	#identify the root of the xml file
-        LRG_root = lrg_tree.getroot()
-
-        #this line of code extracts the required metadata and saves it into a dictionary
-        metadataDict = {'LRG_ID':LRG_root[0][0].text, 'Organism':LRG_root[0][3].text, 'Gene' : LRG_root[0][4][0].text}
-        #check if the value generated by the LRG_ID is 'lrg'
-        tag = str(metadataDict.values()[0])
-        print tag
-        #this line of code extracts the genomic sequence of gene in the LRG file and stores it as a list (CM)
-        GeneSequence = list(LRG_root[0][7].text)
-        #check - there are only ATCGs
-        #assertion - check the length of the list is the same as the length of the genomic sequence by
-        #checking that the end of the coordinate tag 0 and attribute 2 plus 2000 is equal the length 
-	"""The code then executes all of the above functions on every LRG_n.xml file within the current directory"""
-	#execue the functions on each the parsed xml files, these function calls are within the initial for loop
-	exon_dict = LRG_exon_coordinates(LRG_tree)
-	intron_dict = LRG_intron_coordinates(exon_dict)
-	intron_sequences = LRG_intron_sequence(intron_dict)
-	xml_output = xml_from_dict(tag, intron_sequences)
+	lrg.get_name()
+	lrg.get_format()
+	lrg.get_id()
+	lrg.get_exon_coords()
+	lrg.get_intron_coords()
+	lrg.get_sequences()
 	
+	lrg.name
+	lrg.id 
 
-
-'''
